2680721ecdba1bdf488b8c127aba2e8ab5847585
PaoloGuillenLuna <129306198+GrandProgTest@users.noreply.github.com>
2024-09-04 13:43:03 -0500

    Update RealizarRentaPruebaIntegral.java

--- rentstate.project/src/test/java/com/rentstate/tests/rentstate/project/RealizarRentaPruebaIntegral.java
+++ rentstate.project/src/test/java/com/rentstate/tests/rentstate/project/RealizarRentaPruebaIntegral.java
@@ -1,26 +1,56 @@
 package com.rentstate.tests.rentstate.project;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+@ExtendWith(MockitoExtension.class)
 public class RealizarRentaPruebaIntegral {
-	
-	@Mock User renter;
-	@Mock User author;
-	@InjectMocks Property property = new Property("Propiedad 1", 
-			"DescripcionPrueba",
-			"CaracteriticasPruebas", 
-			"UbicacionPrueba",
-			new Category(), true, false, "url1", renter, author, 100.0);
-	
+
+    @Mock
+    User renter;
+
+    @Mock
+    User author;
+
+    @InjectMocks
+    Property property = new Property(
+            "Departamento en el centro", 
+            "Departamento de 2 habitaciones", 
+            "2 habitaciones, 1 baño", 
+            "Centro", 
+            new Category(), 
+            true, 
+            true, 
+            "url-imagen", 
+            null, 
+            author, 
+            100.0 
+        );
+
     @Test
     void puedeRentarConDineroUsuarioYPropiedad() {
-        
+        Mockito.when(renter.getName()).thenReturn("Roberto");
+        Mockito.when(renter.getMoney()).thenReturn(70.0); 
+        Mockito.when(renter.getIsPremium()).thenReturn(true); 
+
+        boolean rentStatus = property.checkRentStatus(property.getIsPosted(), property.getAvailable());
+        assertTrue(rentStatus);
+
+        boolean fundsCheck = Property.renterHasFundsForPropertyRent(renter, property);
+        assertTrue(fundsCheck);
+
+        boolean result = property.rentProperty(renter, property.getAvailable());
+
+        assertTrue(result);
+
+        assertEquals(renter, property.getRenter());
     }
 }
